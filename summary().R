c70f6896680ccb8d5b8d9b74564b09c698d0fdb7
eden dadon <edenvvv@gmail.com>
2019-12-18 21:25:08 +0200

    Get more information as a summary

---
+++ summary().R
@@ -0,0 +1,25 @@
+
+
+# INSTALL AND LOAD PACKAGES ################################
+
+library(datasets)  # Load/unload base packages manually
+
+# LOAD DATA ################################################
+
+head(iris)
+
+# SUMMARY() ################################################
+
+summary(iris$Species)       # Categorical variable
+summary(iris$Sepal.Length)  # Quantitative variable
+summary(iris)               # Entire data frame
+
+# CLEAN UP #################################################
+
+# Clear packages
+detach("package:datasets", unload = TRUE)   # For base
+
+# Clear console
+cat("\014")  # ctrl+L
+
+# Clear mind :)
